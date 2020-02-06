be6f44f47b47cb21909d548bc7b33a1debc03a95
MrSerra <posefa11@gmail.com>
2020-02-06 18:09:04 +0100

    k

---
+++ Assets/Scripts/PJ/PlayerJump.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJump : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [Range(0, 1000)]
+    public float jumpForce = 500f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public int maxNumOfJumps = 1;
+
+    private int numOfJumps = 0;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    [SerializeField]
+    private Transform checkGround;
+
+    private bool isGrounded;
+    public bool IsGrounded => isGrounded;
+
+    private float checkRadius = 0.1f;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        CheckGrounded();
+    }
+
+    private void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(checkGround.position, checkRadius, whatIsGround);        
+    }
+
+    public void OnJump()
+    {
+        if (isGrounded)
+        {
+            numOfJumps = 0;
+        }
+
+        if (numOfJumps <= maxNumOfJumps)
+        {
+            rb2d.velocity = Vector2.up * jumpForce;
+
+            if (rb2d.velocity.y < 0)
+            {
+                rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;                
+            }
+            numOfJumps++;            
+        }
+        
+    }
+
+    public void OnJumpRelease()
+    {       
+        if (rb2d.velocity.y > 0)
+        {
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PJ/raycastDetection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastDetection : MonoBehaviour
+{
+
+    public Transform originPoint;
+    private Vector3 dir = new Vector3(0,-1,0);
+    public float range;
+    public float speed;
+    
+
+    Rigidbody2D rb2D;
+
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+      
+    }
+    void FixedUpdate()
+    {
+        // Cast a ray straight down.
+        RaycastHit2D hit = Physics2D.Raycast(originPoint.position, dir, range);
+        Debug.DrawRay(originPoint.position, dir * range);
+
+        //if (hit.collider.CompareTag("Player"))
+        //{
+        rb2D.AddForce(Vector3.down * speed);
+        //}
+    }
+}
